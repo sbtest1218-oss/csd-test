a644c2648ee321e8eccc035abb5e3283d86d8644
aa <your.email@example.com>
2025-11-08 02:48:28 +0900

    first commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
 Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
 Route::post('/todos/{id}/complete', [TodoController::class, 'complete'])->name('todos.complete');
